52543d9163e6593a8f9a6e6be6ffdefdee4b3e5c
Rohan Verma <rv725555@gmail.com>
2022-08-24 13:53:09 +0530

    feat: Added hello world program.

---
+++ work/hello_world.v
@@ -0,0 +1,8 @@
+module hello_world; // Name of module same as name of file
+    initial
+        begin
+            $display("Hello World!");
+            $finish ;
+        end
+    endmodule
+        
