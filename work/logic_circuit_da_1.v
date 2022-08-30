0f2041da7bf81bc3de9e2a2fdb7c33c02c95f461
Rohan Verma <rv725555@gmail.com>
2022-08-30 18:56:34 +0530

    improv: Improved naming style and added inline documentation.

--- work/logic_circuit_da_1.v
+++ work/logic_circuit_da_1.v
@@ -1,49 +1,46 @@
-module modelthree(f, a, b, c, d);
+module logic_circuit_da_1(f, a, b, c, d);
   input a, b, c, d;
-  output f;  
+  output out;  
   
-  wire i,j,k,l,m;
+  wire wire_1, wire_2, wire_3;
  
-  xor(j, a, b);
+  xor(wire_1, a, b);
   
-  not(i, a);
+  and(wire_2, ~a, c);
   
-  and(k, i, c);
+  nand(wire_3, b, d);
   
-  nand(l, b, d);
-  
-  or(m, j, k);
-  
-  or(f, m, l);
+  or(out, wire_1, wire_2, wire_3);
 
 endmodule
 
-module testbenchformodelthree;
+module test_bench_for_da_1;
   reg a, b, c, d;
   wire out;
-  modelthree m1(out, a, b, c, d); // we are creating an object h1 using half_adder
-  initial
-  begin
+
+  logic_circuit_da_1 test_instance(out, a, b, c, d); // we are creating an object `test_instance` using the module `logic_circuit_da_1`
+  initial begin
   // change default runtime from Simulate --> Runtime options 
   // #100 means perform the operation after 100 ps.
   
-  // 1'b0 is register 0 , 1'b1 is register 1
-  #25    a = 1'b0 ; b = 1'b0; c = 1'b0; d = 1'b0;
-  #25    a = 1'b1 ; b = 1'b0; c = 1'b0; d = 1'b0;
-  #25    a = 1'b0 ; b = 1'b1; c = 1'b0; d = 1'b0;
-  #25    a = 1'b1 ; b = 1'b1; c = 1'b0; d = 1'b0;
-  #25    a = 1'b0 ; b = 1'b0; c = 1'b1; d = 1'b0;
-  #25    a = 1'b1 ; b = 1'b0; c = 1'b1; d = 1'b0;
-  #25    a = 1'b0 ; b = 1'b1; c = 1'b1; d = 1'b0;
-  #25    a = 1'b1 ; b = 1'b1; c = 1'b1; d = 1'b0;
-  #25    a = 1'b0 ; b = 1'b0; c = 1'b0; d = 1'b1;
-  #25    a = 1'b1 ; b = 1'b0; c = 1'b0; d = 1'b1;
-  #25    a = 1'b0 ; b = 1'b1; c = 1'b0; d = 1'b1;
-  #25    a = 1'b1 ; b = 1'b1; c = 1'b0; d = 1'b1;
-  #25    a = 1'b0 ; b = 1'b0; c = 1'b1; d = 1'b1;
-  #25    a = 1'b1 ; b = 1'b0; c = 1'b1; d = 1'b1;
-  #25    a = 1'b0 ; b = 1'b1; c = 1'b1; d = 1'b1;
-  #25    a = 1'b1 ; b = 1'b1; c = 1'b1; d = 1'b1;
+  // 1'b means bit-size of 1, with binary radix. 0 and 1 after that are the values.
+    #25    a = 1'b0 ; b = 1'b0; c = 1'b0; d = 1'b0;
+    #25    a = 1'b1 ; b = 1'b0; c = 1'b0; d = 1'b0;
+    #25    a = 1'b0 ; b = 1'b1; c = 1'b0; d = 1'b0;
+    #25    a = 1'b1 ; b = 1'b1; c = 1'b0; d = 1'b0;
+    #25    a = 1'b0 ; b = 1'b0; c = 1'b1; d = 1'b0;
+    #25    a = 1'b1 ; b = 1'b0; c = 1'b1; d = 1'b0;
+    #25    a = 1'b0 ; b = 1'b1; c = 1'b1; d = 1'b0;
+    #25    a = 1'b1 ; b = 1'b1; c = 1'b1; d = 1'b0;
+    #25    a = 1'b0 ; b = 1'b0; c = 1'b0; d = 1'b1;
+    #25    a = 1'b1 ; b = 1'b0; c = 1'b0; d = 1'b1;
+    #25    a = 1'b0 ; b = 1'b1; c = 1'b0; d = 1'b1;
+    #25    a = 1'b1 ; b = 1'b1; c = 1'b0; d = 1'b1;
+    #25    a = 1'b0 ; b = 1'b0; c = 1'b1; d = 1'b1;
+    #25    a = 1'b1 ; b = 1'b0; c = 1'b1; d = 1'b1;
+    #25    a = 1'b0 ; b = 1'b1; c = 1'b1; d = 1'b1;
+    #25    a = 1'b1 ; b = 1'b1; c = 1'b1; d = 1'b1;
+    #25    $finish
   end
 endmodule
 
