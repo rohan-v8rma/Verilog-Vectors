7a94240a659fa9fc51f361678885cf58d943f5c1
Rohan Verma <rv725555@gmail.com>
2022-09-13 23:48:04 +0530

    feat: Scripts for 4-to-1 and 8-to-1 multiplexers using vectors.

---
+++ multiplexer_4_to_1.v
@@ -0,0 +1,34 @@
+module multiplexer_4_to_1(input_lines, select_lines, out);
+    input[3:0] input_lines; // 4-bit vector for input lines
+    input[1:0] select_lines; // 2-bit vector for select lines
+    
+    output out;
+    reg out;
+    
+    always @(input_lines or select_lines) begin
+      case (select_lines) 
+        2'b00 : out = input_lines[0];
+        2'b01 : out = input_lines[1]; 
+        2'b10 : out = input_lines[2];
+        2'b11 : out = input_lines[3];
+      endcase
+    end
+endmodule
+
+module muxtest_4_to_1;
+    reg[3:0] input_lines;
+    reg[1:0] select_lines;
+    wire out;
+    multiplexer_4_to_1 wire_driver(input_lines, select_lines, out);
+    
+    initial begin
+          
+          input_lines = 4'b0101; select_lines = 2'b00;
+      #20 select_lines = 2'b01;
+      #20 select_lines = 2'b10;
+      #20 select_lines = 2'b11;
+      
+      
+      #20 $finish;
+    end
+endmodule
---
+++ multiplexer_8_to_1.v
@@ -0,0 +1,42 @@
+module multiplexer_8_to_1(input_lines, select_lines, out);
+    input[7:0] input_lines; // 8-bit vector for input lines
+    input[2:0] select_lines; // 3-bit vector for select lines
+    
+    output out;
+    reg out;
+    
+    always @(input_lines or select_lines) begin
+      case (select_lines) 
+        3'b000 : out = input_lines[0];
+        3'b001 : out = input_lines[1]; 
+        3'b010 : out = input_lines[2];
+        3'b011 : out = input_lines[3];
+        3'b100 : out = input_lines[4];
+        3'b101 : out = input_lines[5]; 
+        3'b110 : out = input_lines[6];
+        3'b111 : out = input_lines[7];
+      endcase
+    end
+endmodule
+
+module muxtest_8_to_1;
+    reg[7:0] input_lines;
+    reg[2:0] select_lines;
+    wire out;
+    multiplexer_8_to_1 wire_driver(input_lines, select_lines, out);
+    
+    initial begin
+          
+          input_lines = 8'b01110001; 
+          select_lines = 3'b000;
+      #20 select_lines = 3'b001;
+      #20 select_lines = 3'b010;
+      #20 select_lines = 3'b011;
+      #20 select_lines = 3'b100;
+      #20 select_lines = 3'b101;
+      #20 select_lines = 3'b110;
+      #20 select_lines = 3'b111;
+      #20 $finish;
+    end
+      
+endmodule
